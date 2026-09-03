3f73baba3dc3f3f3784c638a5c04410b575d0936
mustapha abourar <elmustapha.abourar@outlook.com>
2026-09-03 19:54:58 +0200

    feat(api): setup Hono framework server entrypoint in Next.js

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,35 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { clerkMiddleware } from "@clerk/hono";
+import images from "@/app/api/[[...route]]/images";
+import ai from "@/app/api/[[...route]]/ai";
+import agent from "@/app/api/[[...route]]/agent";
+import projects from "@/app/api/[[...route]]/projects";
+import subscriptions from "@/app/api/[[...route]]/subscriptions";
+
+export const runtime = "nodejs";
+
+const app = new Hono().basePath("/api");
+
+app.use(
+  "*",
+  clerkMiddleware({
+    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    secretKey: process.env.CLERK_SECRET_KEY,
+  })
+);
+
+const routes = app
+  .route("/ai", ai)
+  .route("/agent", agent)
+  .route("/images", images)
+  .route("/projects", projects)
+  .route("/subscriptions", subscriptions);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof routes;
